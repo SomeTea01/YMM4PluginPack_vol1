40bf9e3fd48e2fcca250927a83ddeb423ac94b86
SomeTea01 <Tea0001024@gmail.com>
2024-02-16 18:14:06 +0900

    リリース用資材の容易のため

--- TestEnv/Resources/Tool/32bitOSで起動するようにする.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-copy /Y "..\AnyCPU\YukkuriMovieMaker.deps.json" "..\..\YukkuriMovieMaker.deps.json"
-copy /Y "..\AnyCPU\YukkuriMovieMaker.runtimeconfig.json" "..\..\YukkuriMovieMaker.runtimeconfig.json"
