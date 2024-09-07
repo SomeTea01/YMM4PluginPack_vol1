9a8ec088ad7ddf8232b529d1f78fc82eba4a4c31
SomeTea01 <Tea0001024@gmail.com>
2024-09-07 11:52:09 +0900

    Revert "実行環境の削除"
    This reverts commit 7adc6222121288e21555346350c610fc7b606f95.

---
+++ TestEnv/Resources/Tool/32bitOSで起動するようにする.bat
@@ -0,0 +1,3 @@
+@echo off
+copy /Y "..\AnyCPU\YukkuriMovieMaker.deps.json" "..\..\YukkuriMovieMaker.deps.json"
+copy /Y "..\AnyCPU\YukkuriMovieMaker.runtimeconfig.json" "..\..\YukkuriMovieMaker.runtimeconfig.json"
